a076f34a3cf06586d4f40caccf2494e8cf9d348d
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-19 10:22:09 -0300

    Reorganização e segunda parte
    separei em pastas e fiz o segundo parte do EX

---
+++ Scripts/Script2.py
@@ -0,0 +1,5 @@
+dia = input('Que dia você nasceu?')
+mes = input('Em que mes você nasceu?')
+ano = input('Em que ano você nasceu?')
+
+print('Você nasceu no dia', dia, 'do mes', mes, 'do ano de', ano, 'correto?')
